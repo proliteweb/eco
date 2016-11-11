1a720f72913dc53d384203fb7bb9a8b196d03370
oleg <oleg.dyachenko1993@gmail.com>
2016-11-11 11:50:50 +0200

    +

--- asd.php
+++ asd.php
@@ -6,19 +6,26 @@ $table = "posts";
 //
 //$user_id = ($_COOKIE["ID"]) ? $_COOKIE["ID"] : null;
 
-for($i = 0; $i <= 200000; $i++){
-    $date = mktime();
-    $income = rand(0, 999);
-    $outgo = rand(0, 999);
-    $user_id = rand(1, 99);
-
-    $array = [
-        "date"=>$date
-        ,"user_id"=>$user_id
-        ,"income"=>$income
-        ,"outgo"=>$outgo
-    ];
-
-    $query = db_insert($table,$array);
-    unset($array);
+//for($i = 0; $i <= 200000; $i++){
+//    $date = mktime();
+//    $income = rand(0, 999);
+//    $outgo = rand(0, 999);
+//    $user_id = rand(1, 99);
+//
+//    $array = [
+//        "date"=>$date
+//        ,"user_id"=>$user_id
+//        ,"income"=>$income
+//        ,"outgo"=>$outgo
+//    ];
+//
+//    $query = db_insert($table,$array);
+//    unset($array);
+//}
+$res = db_query("INSERT INTO posts (`date`, `user_id`, `income`, `outgo`) VALUES (NOW(), 5, 15, 65)");
+if(!$res){
+    echo "no";
+}
+else{
+    echo "success";
 }
