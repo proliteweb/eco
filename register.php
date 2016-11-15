fa7f4a25e1487b00b42c82023a24d0eec347eb81
oleg <oleg.dyachenko1993@gmail.com>
2016-11-15 10:34:24 +0200

    +

--- register.php
+++ register.php
@@ -13,23 +13,23 @@ require_once "functions/cookies.php";
 if($_POST["method_name"] === "register" AND !empty($_POST["method_name"])){
     $for_return = [];
 
-    $name = ( preg_match("/^[a-z]+$/i", $_POST['name']) ) ? proverka1( trim($_POST['name']) ) : null;
-    $email = ($_POST['email']) ? trim($_POST['email']) : "";
-    $password = (preg_match("/^[0-9a-z]+$/i", $_POST['password'])) ? md5( proverka1 (trim($_POST['password']) ) ) : $for_return['error'] = "Не допустимые символы в пароле";
+    $name = ( preg_match("/^[a-zА-я]+$/i", $_POST['name']) ) ? proverka1( trim($_POST['name']) ) : $for_return['error']= "Должны быть только латиница или кирилица в ".$_POST['name'];
+    $email = (filter_var($_POST['email'])) ? trim($_POST['email']) : $for_return['error'] = "Не правильный формат Email ".$_POST['email'];
+    $password = (preg_match("/^[0-9a-z]+$/i", $_POST['password'])) ? md5( proverka1 (trim($_POST['password']) ) ) : $for_return['error'] = "Не допустимые символы в пароле ".$_POST['password'] ;
 
 //    $resDb = db_row("SELECT * FROM users WHERE name='".$name."' AND token='".$password."'", true )["item"];
 
-    if($resDb['name'] !== $name OR $resDb['token'] !== $password){
-        $for_return['error'] = "Не правильный логин или пароль!";
-    }
+//    if($resDb['name'] !== $name OR $resDb['token'] !== $password){
+//        $for_return['error'] = "Не правильный логин или пароль!";
+//    }
     //Если все ОК , то формируем данные для куки)
-    else{
-        $arr_names = [
-            0=>"ID"
-            ,1=>"token"
-        ];
-        $for_return = set_cookie_ajax($arr_names, $resDb);
-    }//endif
+//    else{
+//        $arr_names = [
+//            0=>"ID"
+//            ,1=>"token"
+//        ];
+//        $for_return = set_cookie_ajax($arr_names, $resDb);
+//    }//endif
     //возвращаем результат
     $for_return = json_encode($for_return);
     echo $for_return;
